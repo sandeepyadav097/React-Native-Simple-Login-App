4afd68501fa989ce9ed243a11752a9d9ed1c2c58
sandeepyadav097 <sandeepyadav097@gmail.com>
2018-06-10 18:41:43 +0530

    form and auth completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,24 @@
 import React, { Component} from 'react'
-import {View ,Text}  from 'react-native'
-import { Header, Button , Card, CardItem } from './common'
+import {Text} from 'react-native'
+import { Header, Button , Card, CardItem , Input} from './common'
 
 class Login extends Component{
+
+    state= { email:'', password:'', error:''}; 
+    
+    onButtonPress(){
+        
+        const firebase = require("firebase");
+        const {email, password}=this.state;
+        firebase.auth().signInWithEmailAndPassword(email,password)
+            .catch( ()=>{
+                firebase.auth().createUserWithEmailAndPassword(email,password)
+                .catch( ()=>{
+                    this.setState({error:'Authentiation Failed'});
+                }) 
+        })
+
+    }
 
     render(){
 
@@ -10,11 +26,23 @@ class Login extends Component{
 
           
             <Card>
-                <CardItem/>
-                <CardItem/>
+                <CardItem>
+                    <Input  label="E-mail" placeholder={'user@example.com'} value={this.state.email} onChangeText={ email => {this.setState({email})}  }      />
+                 
+                    </CardItem>
+                <CardItem>
+                    <Input secureTextEntry label="Password"  placeholder={'password'} value={this.state.password} onChangeText={ password => {this.setState({password})}  }      />
+                 
+                 </CardItem>  
+
+                  <Text style={style.errorStyle} >{this.state.error}
+                 </Text>   
+
                 <CardItem>
 
-                      <Button>
+                
+
+                      <Button onPress={ this.onButtonPress.bind(this) }>
                           Log In 
                           </Button>
                  </CardItem>   
@@ -27,5 +55,15 @@ class Login extends Component{
 
 
 } 
+
+const style={
+    errorStyle:{
+        fontSize:20,
+        alignSelf:'center',
+        color:'red'
+    }
+}
+
+
 
 export default Login;
